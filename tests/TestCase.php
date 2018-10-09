83dd05349166f7244e4f449469da6879988f0c20
simialbi <simi.albi@gmail.com>
2018-10-09 14:37:00 +0200

    changed project structure to prepare test suite and prepared test suite

---
+++ tests/TestCase.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package yii2-rest-client
+ * @author Simon Karlen <simi.albi@gmail.com>
+ */
+
+namespace yiiunit\extensions\rest;
+
+use Yii;
+use yii\di\Container;
+use yii\helpers\ArrayHelper;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        $this->destroyApplication();
+    }
+
+
+    /**
+     * @param array $config
+     * @param string $appClass
+     */
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+            'aliases' => [
+                '@bower' => '@vendor/bower-asset',
+                '@npm' => '@vendor/npm-asset',
+            ],
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
+                    'scriptFile' => __DIR__ . '/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+                'rest' => [
+                    'class' => 'simialbi\yii2\rest\Connection',
+                    'baseUrl' => 'https:s//api.site.com/'
+                ]
+            ]
+        ], $config));
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        Yii::$app = null;
+        Yii::$container = new Container();
+    }
+}
